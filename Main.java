2a2d86c4437afd14d4f06972c46e14f057d62b61
ChenFeng <1041958497@qq.com>
2019-02-03 16:51:41 +0800

    UI update safely

--- Main.java
+++ Main.java
@@ -8,18 +8,25 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import java.util.*;
 import javafx.concurrent.*;
+import javafx.beans.property.*;
+
 
 public class Main extends Application {
 
     Stage window;
-	ObservableList<Process> prosRunning = FXCollections.observableArrayList();
-	Scheduler duler = new Scheduler(prosRunning);
-	HashMap<String, List<Process>> pros;
+	// ObservableList<Process> pros = FXCollections.observableArrayList();
+	// ObservableList<Process> running = FXCollections.observableArrayList();
+	// ObservableList<Process> ready = FXCollections.observableArrayList();
+	// ObservableList<Process> blocked = FXCollections.observableArrayList();
+	Scheduler duler = new Scheduler();
+
 	
 	@Override
     public void start(Stage primaryStage)  {
         initGui(primaryStage);
-		(new Thread(duler)).start();
+		Thread t = new Thread(duler);
+		t.setDaemon(true);
+		t.start();
 	}
 
    
@@ -28,16 +35,22 @@ public class Main extends Application {
         window.setTitle("OS");
 		
 		
-        Button button = new Button("rank");
-		button.setOnAction(e -> duler.buttonClicked());
+        Button button = new Button("Sort");
+		button.setOnAction(e -> duler.sort());
 
-		HBox hb = new HBox();
-		ListView<Process> prosRunningList = new ListView<>();
-		prosRunningList.setItems(prosRunning);
-        prosRunningList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-		prosRunningList.setPadding(new Insets(30,30,30,30));
+		VBox listBox = new VBox();
+		listBox.setSpacing(5);
+		listBox.setPadding(new Insets(10,10,10,10));
 
+		ListView<Process> runningList = listFactory(duler.running);
+		ListView<Process> readyList = listFactory(duler.ready);
+		ListView<Process> blockedList = listFactory(duler.blocked);
+		Label lbRunning = new Label("Running:");
+		Label lbReady = new Label("Ready:");
+		Label lbBlocked = new Label("Blocked:");
+		listBox.getChildren().addAll(lbRunning, runningList,lbReady, readyList,lbBlocked, blockedList);
 
+		
 		
 		ListView<String> instsList = new ListView<>();
 		ObservableList insts = FXCollections.observableArrayList("");
@@ -49,14 +62,15 @@ public class Main extends Application {
 		VBox vb = new VBox();
 		vb.setPadding(new Insets(30,30,30,30));
 		Label lb1 = new Label("hi");
-		Label lb2 = new Label("bye");
+		Label lb2 = new Label();
+		lb2.textProperty().bind(duler.messageProperty());
 		lb1.setPrefWidth(100);
 		lb2.setPrefWidth(100);
 		vb.getChildren().addAll(lb1, lb2);
 		
 		BorderPane bd = new BorderPane();
 		bd.setPadding(new Insets(30,30,30,30));
-		bd.setCenter(listView);
+		bd.setCenter(listBox);
 		bd.setRight(instsList);
 		bd.setTop(button);
 		
@@ -67,11 +81,19 @@ public class Main extends Application {
         window.show();
     }
 
+	ListView<Process> listFactory(ObservableList<Process> items) {
+		ListView<Process> list = new ListView<>();
+		list.itemsProperty().bindBidirectional(new SimpleListProperty<Process>(items));
+        list.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		return list;
+	}
+
     
    
 	public static void main(String[] args) {
         launch(args);
     }
+	
 }
 
 
@@ -79,36 +101,58 @@ public class Main extends Application {
 
 class Scheduler extends Task<Void> {
 
+	ObservableList<Process> running;
+	ObservableList<Process> ready;
+	ObservableList<Process> blocked;
 	ObservableList<Process> pros;
 	
-	Scheduler(ObservableList<Process> pros) {
-		this.pros = pros;
+	
+	Scheduler() {
+		this.pros = FXCollections.observableArrayList();
+		this.running = FXCollections.observableArrayList();
+		this.ready = FXCollections.observableArrayList();
+		this.blocked = FXCollections.observableArrayList();
 	}
 
 	@Override
 	protected Void call() {
-	
 		for(int i=1; i<10 ; i++) {
-			try{
-				Thread.sleep(1000);
-			} catch(Exception e) {}
 			Process p = new Process(i);
-			p.setPri(i);
 			pros.addAll(p);
-			
 		}
+		running.addAll(pros.get(0));
+		ready.addAll(pros);
+		ready.remove(0);
+		sched();
+		
 		return null;
 	}
-	void buttonClicked(){
-		Platform.runLater( new Runnable() {
-				@Override
-				public void run() {
-					for(int i=0; i<3 ; i++) {
-						pros.get(i).setPri(3-i);
+
+	void sched() {
+		while(true) {
+			try{ Thread.sleep(1000);
+			} catch(InterruptedException e) {}
+			Platform.runLater(new Runnable() {
+					@Override
+					public void run() {
+						FXCollections.sort(ready);
+						Process nextRunning = ready.remove(0);
+						Process quitRunning = running.remove(0);
+						
+						running.add(nextRunning);
+			
+						ready.add(quitRunning);
+						
 					}
-					FXCollections.sort(pros);	
-				}
-			} );
+				});
+		}
+		
+	}
+	void sort(){
+		FXCollections.reverse(pros);	
+		FXCollections.reverse(running);	
+		FXCollections.reverse(ready);	
+		FXCollections.reverse(blocked);	
     }
 }
 
@@ -118,6 +162,8 @@ class Process implements Comparable<Process> {
 	int pc = 0;
 	int pid;
 	int pri;
+	double vruntime;
+	int runtime;
 	Process(int pid) {
 		this.pid = pid;
 	}
@@ -125,18 +171,17 @@ class Process implements Comparable<Process> {
 	void setPri(int pri) {
 		this.pri = pri;
 	}
-	
-	
-	public void run() {
-		String [] strs = {"1", "2", "3", "4"};
-		try{
-			// while(true) {
-			// 	inst = strs[pc];
-			// 	pc = (pc+1) % 4;
-			// }
-			while(true);
-		} catch(Exception e) {return;}
+	synchronized void run() {
+		try{ Thread.sleep(300);
+		} catch(InterruptedException e) {}
+		System.out.println("Process "+pid+"run ");
+		runtime--;
+		vruntime = runtime*pri;
+		
 	}
+	
+	
+
 
 	@Override
 	public String toString() {
@@ -145,6 +190,6 @@ class Process implements Comparable<Process> {
 
 	@Override
 	public int compareTo(Process o) {
-		return this.pri - o.pri;
+		return - (this.vruntime - o.vruntime);
 	}
 }
